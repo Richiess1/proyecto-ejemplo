2a84b2a88d58f0388984e36e5ca38520d7d641a3
Richie <20235894@esen.edu.sv>
2025-06-23 19:01:59 -0600

    feat: add ProductsService and its test suite to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MathService } from './math/math.service';
+import { ProductsService } from './products/products.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, MathService],
+  providers: [AppService, MathService, ProductsService],
 })
 export class AppModule {}
